6192adff99aa6b866f3e29d577b063be02c471e3
Malak2026CodeSecure <malak0097222@gmail.com>
2026-08-28 21:22:18 +0300

    Create Tournament.cpp

---
+++ Tournament.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+
+        int n, j, k;
+        cin >> n >> j >> k;
+        int a[n];
+        for (int i = 0; i < n; i++)
+            cin >> a[i];
+        int max = a[0];
+        for (int i = 1; i < n; i++)
+        {
+            if (a[i] > max)
+                max = a[i];
+        }
+
+        if (k == 1 && a[j - 1] != max)
+            cout << "NO" << endl;
+        else
+            cout << "YES" << endl;
+    }
+}
